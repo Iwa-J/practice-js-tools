6e188da542599ff4c70c758ed6ca9767b6a8707f
Iwona Juraszek <kamin.iwo@gmail.com>
2024-05-26 16:24:14 +0200

    add node version

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,3 @@
 console.log('Działa! Gratuluję uruchomienia JS z poziomu Node.js!');
-console.log('Moja wersja Node.js to: [v20.13.1]');
-console.log('Moja wersja npm to: [10.5.2]');
+console.log('Moja wersja Node.js to: v20.13.1');
+console.log('Moja wersja npm to: 10.5.2');
